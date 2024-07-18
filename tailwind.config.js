7c26eea1078694008e652706b365b22b7150716f
Edgarm <edgar.martinez@canteradigital.mx>
2024-07-17 20:51:54 -0600

    Add: Add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ['./index.html', './src/**/*.{ts,tsx}'],
   theme: {
+    colors: {
+      primary: '#0A2342',
+      secondary: '#F46197',
+      font_primary: '#FFFDF7',
+    },
     extend: {},
   },
   plugins: [],
+  darkMode: 'selector',
 };
