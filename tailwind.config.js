4ab7abda35f6541c7414f9f377b6787529afe710
Edgarm <edgar.martinez@canteradigital.mx>
2024-07-20 00:10:29 -0600

    Update - update tailwind config to set new colors for both theme modes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     colors: {
       primary: '#0A2342',
       secondary: '#F46197',
+      tertiary: '#FFFDF7',
       font_primary: '#FFFDF7',
+
+      primaryLight: '#FFFDF7',
+      secondaryLight: '#F46197',
+      tertiaryLight: '#FFFDF7',
+
+      font_primaryLight: '#000000',
     },
     extend: {},
   },
