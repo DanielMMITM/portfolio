6a6e659b7a356285fff999106e77490c8f2673cf
Edgarm <edgar.martinez@canteradigital.mx>
2024-07-21 15:09:15 -0600

    Update - update classname colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+import { Description } from './components/Description';
 import { Header } from './components/Header';
 
 export const App = () => {
   return (
-    <div className="bg-primaryLight text-font_primaryLight dark:bg-primary dark:text-font_primary border-3 min-h-[100dvh]">
+    <div className="bg-primaryLight text-tertiaryLight dark:bg-primary dark:text-tertiary border-3 min-h-[100dvh]">
       <div className="flex flex-col p-6 mx-10 md:mx-44 gap-y-7 border-2">
         <Header />
-        <h1>Edgar Martinez</h1>
+        <Description />
       </div>
     </div>
   );
