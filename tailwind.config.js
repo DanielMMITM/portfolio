bfff716cf83ce84fa2a1b634ae3300207e42f36f
Edgarm <edgar.martinez@canteradigital.mx>
2024-07-21 15:08:36 -0600

    Update - update color names and new transparent color

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,11 @@ export default {
       primary: '#0A2342',
       secondary: '#F46197',
       tertiary: '#FFFDF7',
-      font_primary: '#FFFDF7',
+      transparent: 'transparent',
 
       primaryLight: '#FFFDF7',
-      secondaryLight: '#F46197',
-      tertiaryLight: '#FFFDF7',
-
-      font_primaryLight: '#000000',
+      secondaryLight: '#2364AA',
+      tertiaryLight: '#000000',
     },
     extend: {},
   },
