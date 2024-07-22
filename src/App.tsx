89e1b450edd71563f19f994cab3b1c50f85ea592
Edgarm <edgar.martinez@canteradigital.mx>
2024-07-22 00:00:30 -0600

    Add - add context to the component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { Description } from './components/Description';
 import { Header } from './components/Header';
+import { AppContextProvider } from './context/AppContext';
 
 export const App = () => {
   return (
-    <div className="bg-primaryLight text-tertiaryLight dark:bg-primary dark:text-tertiary border-3 min-h-[100dvh]">
-      <div className="flex flex-col p-6 mx-10 md:mx-44 gap-y-7 border-2">
-        <Header />
-        <Description />
+    <AppContextProvider>
+      <div className="bg-primaryLight text-tertiaryLight dark:bg-primary dark:text-tertiary border-3 min-h-[100dvh]">
+        <div className="flex flex-col p-6 mx-10 md:mx-44 gap-y-7 border-2">
+          <Header />
+          <Description />
+        </div>
       </div>
-    </div>
+    </AppContextProvider>
   );
 };
 
