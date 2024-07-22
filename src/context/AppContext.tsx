47628742717806241f3678ad388ba35bd3286524
Edgarm <edgar.martinez@canteradigital.mx>
2024-07-22 00:00:06 -0600

    Add - add context to replace hooks and handle theme mode and preferred language

---
+++ src/context/AppContext.tsx
@@ -0,0 +1,54 @@
+import { createContext, ReactNode, useContext, useState } from 'react';
+
+interface AppContextProps {
+  children: ReactNode;
+}
+
+type AppContextConfigType = {
+  english: boolean;
+  toggleLanguage: () => void;
+  darkMode: boolean;
+  toggleDarkMode: () => void;
+};
+
+const initialAppContextConfigType: AppContextConfigType = {
+  english: true,
+  toggleLanguage: () => {},
+  darkMode: true,
+  toggleDarkMode: () => {},
+};
+
+export const AppContextProvider = ({ children }: AppContextProps) => {
+  const [english, setEnglish] = useState(true);
+  const [darkMode, setDarkMode] = useState(true);
+
+  const toggleLanguage = () => {
+    setEnglish(!english);
+  };
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        english,
+        toggleLanguage,
+        darkMode,
+        toggleDarkMode,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const AppContext = createContext<AppContextConfigType>(
+  initialAppContextConfigType
+);
+
+export const useAppContext = () => {
+  const context = useContext(AppContext);
+  return context;
+};
