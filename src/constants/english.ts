5453db862b3d562a23b027fecbfd819e9c4082c4
Edgarm <edgar.martinez@canteradigital.mx>
2024-09-04 00:18:10 -0600

    Add - add tabs title constants

--- src/constants/english.ts
+++ src/constants/english.ts
@@ -1,2 +1,5 @@
 export const SELF_DESCRIPTION_EN =
   "I'm a Computer Systems Engineering student at Instituto Tecnológico de Morelia about to graduate. I considermyself creative, meticulous and passionate about my career. I like to learn new things and keep improving myhabilities. I have learned by myself the basics of React, Python and Spring. I have also worked with severalprogramming languages and frameworks. Currently I'm still learning about AWS.";
+export const EXPERIENCE = 'Experience';
+export const PROJECTS = 'Personal Projects';
+export const TECHNOLOGIES = 'Technologies';
--- src/constants/spanish.ts
+++ src/constants/spanish.ts
@@ -1,1 +1,4 @@
 export const SELF_DESCRIPTION_ES = '';
+export const EXPERIENCIA = 'Experiencia';
+export const PROYECTOS = 'Proyectos Personales';
+export const TECNOLOGIAS = 'Tecnologías';
