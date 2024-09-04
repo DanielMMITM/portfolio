cd9ca1c6c231480be547580995c599eb87356950
Edgarm <edgar.martinez@canteradigital.mx>
2024-09-03 21:08:06 -0600

    Update - update margin layout

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ export const App = () => {
   return (
     <AppContextProvider>
       <div className="bg-primaryLight text-tertiaryLight dark:bg-primary dark:text-tertiary border-3 min-h-[100dvh]">
-        <div className="flex flex-col p-6 mx-10 md:mx-44 gap-y-7 border-2">
+        <div className="flex flex-col p-6 mx-8 md:mx-40 gap-y-7 border-2">
           <Header />
           <Description />
         </div>
