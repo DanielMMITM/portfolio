25f69a9f1035168231e04ff64f1623ede28604b6
Edgarm <edgar.martinez@canteradigital.mx>
2024-09-04 00:55:36 -0600

    Add - add style to handle selected tab status

--- src/components/TabsInfo.tsx
+++ src/components/TabsInfo.tsx
@@ -1,30 +1,35 @@
+import { useState } from 'react';
 import { EXPERIENCE, PROJECTS, TECHNOLOGIES } from '../constants/english';
 import { EXPERIENCIA, PROYECTOS, TECNOLOGIAS } from '../constants/spanish';
 import { useAppContext } from '../context/AppContext';
 
 export const TabsInfo = () => {
   const { english } = useAppContext();
+  const [selectedTab, setSelectedTab] = useState(1);
 
   return (
     <div className="flex justify-center">
       <button
         role="tab"
         type="button"
-        className="relative p-3 hover:bg-primaryLight hover:bg-opacity-5 after:h-1 after:w-1 after:bg-primary after:dark:bg-secondary custom-transition rounded-lg"
+        className={`${selectedTab === 1 && 'custom-transition-force'} relative p-3 hover:bg-primaryLight hover:bg-opacity-5 after:h-1 after:w-1 after:bg-primary after:dark:bg-secondary custom-transition rounded-lg`}
+        onClick={() => setSelectedTab(1)}
       >
         {english ? EXPERIENCE : EXPERIENCIA}
       </button>
       <button
         role="tab"
         type="button"
-        className="relative p-3 hover:bg-primaryLight hover:bg-opacity-5 after:h-1 after:w-1 after:bg-primary after:dark:bg-secondary custom-transition rounded-lg"
+        className={`${selectedTab === 2 && 'custom-transition-force'} relative p-3 hover:bg-primaryLight hover:bg-opacity-5 after:h-1 after:w-1 after:bg-primary after:dark:bg-secondary custom-transition rounded-lg`}
+        onClick={() => setSelectedTab(2)}
       >
         {english ? PROJECTS : PROYECTOS}
       </button>
       <button
         role="tab"
         type="button"
-        className="relative p-3 hover:bg-primaryLight hover:bg-opacity-5 after:h-1 after:w-1 after:bg-primary after:dark:bg-secondary custom-transition rounded-lg"
+        className={`${selectedTab === 3 && 'custom-transition-force'} relative p-3 hover:bg-primaryLight hover:bg-opacity-5 after:h-1 after:w-1 after:bg-primary after:dark:bg-secondary custom-transition rounded-lg`}
+        onClick={() => setSelectedTab(3)}
       >
         {english ? TECHNOLOGIES : TECNOLOGIAS}
       </button>
