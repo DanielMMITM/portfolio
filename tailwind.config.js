ca6eecfa011fe8319e389bf16f6aa8e54772b3b6
Edgarm <edgar.martinez@canteradigital.mx>
2024-09-03 20:51:49 -0600

    Add - set custom breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,13 @@ export default {
       tertiaryLight: '#000000',
     },
     extend: {},
+    screens: {
+      xs: '250px',
+      sm: '600px',
+      md: '900px',
+      lg: '1280px',
+      xl: '2400px',
+    },
   },
   plugins: [],
   darkMode: 'selector',
