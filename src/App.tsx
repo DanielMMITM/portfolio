f1b22fcc5fc13af2cee876d2339bd598c8bc5edc
Edgarm <edgar.martinez@canteradigital.mx>
2024-09-11 00:18:46 -0600

    Update - update meteors style and quantity and delete unused div

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,12 @@ document.documentElement.classList.toggle('dark');
 export const App = () => {
   return (
     <AppContextProvider>
-      <div className="relative flex w-full flex-col overflow-hidden  text-tertiaryLight dark:bg-primary dark:text-tertiary border-3 min-h-[100dvh] ">
-        <div className="">
-          <div className="flex flex-col p-6 mx-8 md:mx-40 gap-y-7 border-2">
-            <Meteors number={30} />
-            <Header />
-            <Description />
-            <TabsInfo />
-          </div>
+      <div className="relative flex w-full flex-col overflow-hidden text-tertiaryLight dark:bg-primary dark:text-tertiary border-3 min-h-[100dvh] ">
+        <div className="flex flex-col p-6 mx-8 md:mx-40 gap-y-7 border-2">
+          <Meteors number={20} />
+          <Header />
+          <Description />
+          <TabsInfo />
         </div>
       </div>
     </AppContextProvider>
