11b2cd3c8e4ddcc70896e39d6954945e9e29be33
Edgarm <edgar.martinez@canteradigital.mx>
2024-09-11 00:03:34 -0600

    Add - add meteors component and implement it into the main page

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,21 @@ import { Description } from '@/components/Description';
 import { Header } from '@/components/Header';
 import { TabsInfo } from '@/components/TabsInfo';
 import { AppContextProvider } from '@/context/AppContext';
+import Meteors from '@/components/Meteors';
 
 document.documentElement.classList.toggle('dark');
 
 export const App = () => {
   return (
     <AppContextProvider>
-      <div className="bg-primaryLight text-tertiaryLight dark:bg-primary dark:text-tertiary border-3 min-h-[100dvh]">
-        <div className="flex flex-col p-6 mx-8 md:mx-40 gap-y-7 border-2">
-          <Header />
-          <Description />
-          <TabsInfo />
+      <div className="relative flex w-full flex-col overflow-hidden  text-tertiaryLight dark:bg-primary dark:text-tertiary border-3 min-h-[100dvh] ">
+        <div className="">
+          <div className="flex flex-col p-6 mx-8 md:mx-40 gap-y-7 border-2">
+            <Meteors number={30} />
+            <Header />
+            <Description />
+            <TabsInfo />
+          </div>
         </div>
       </div>
     </AppContextProvider>
