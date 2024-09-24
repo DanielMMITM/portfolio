165be45f9fb2557b2a55fbb21adf1a97bf6cf519
Edgarm <edgar.martinez@canteradigital.mx>
2024-09-24 00:39:53 -0600

    Update - update style and structure tags for new data

--- src/components/Tabs/Experience.tsx
+++ src/components/Tabs/Experience.tsx
@@ -22,22 +22,30 @@ export const Experience = () => {
   };
 
   return (
-    <section className="mt-8">
+    <section className="mt-10">
       {experience?.map((data) => (
         <div className="flex" key={data.id}>
           <div>
             <img src={data.logo} alt="employer logo" />
           </div>
-          <div className="flex flex-col">
+          <div className="flex flex-col ml-6">
             <div className="flex gap-2">
               <h1 className="dark:text-secondary text-secondaryLight font-bold">
-                {data.employer} |
+                {data.employer}
               </h1>
               <h2 className="dark:text-secondary text-primary opacity-90 font-semibold">
                 {data.jobTitle}
               </h2>
             </div>
             <h3 className="opacity-70 dark:text-slate-100 text-slate-900 font-semibold">{`${formatDate(data.startDate)} - ${formatDate(data.endDate)}`}</h3>
+            <p>{english ? data.descEn : data.descES}</p>
+            <ul className="list-disc">
+              {data.tasks.map((task) => (
+                <li key={task.id} className="list-item">
+                  {english ? task.taskDescEN : task.taskDescES}
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
       ))}
