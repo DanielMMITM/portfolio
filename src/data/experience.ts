6d6fa82eb864f2a64ba6d5df63eccc231d4a70bf
Edgarm <edgar.martinez@canteradigital.mx>
2024-09-25 00:25:19 -0600

    Update - update data

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -9,58 +9,132 @@ export const experience = [
     descES: '',
     descEn: '',
     tasks: [
-      { id: 1, taskDescES: '', taskDescEN: '' },
-      { id: 2, taskDescES: '', taskDescEN: '' },
-      { id: 3, taskDescES: '', taskDescEN: '' },
-      { id: 4, taskDescES: '', taskDescEN: '' },
+      {
+        id: 1,
+        taskDescES: 'Desarrollo de interfaces y consumo de REST APIS.',
+        taskDescEN: 'Develop software interfaces and consume REST APIS.',
+      },
+      {
+        id: 2,
+        taskDescES:
+          'Uso de librerías para la administración de datos como zustand, tanstack query, etc.',
+        taskDescEN:
+          'Usage of libraries for data management such as zustand, tanstack query, etc.',
+      },
+      {
+        id: 3,
+        taskDescES:
+          'Trabajé con la metodología SCRUM y control de versiones (GIT).',
+        taskDescEN: 'Work with SCRUM methodology and versioning systems (GIT).',
+      },
+      {
+        id: 4,
+        taskDescES: 'Elaboración de documentación técnica del proyecto.',
+        taskDescEN: 'Elaborate technical documentation',
+      },
     ],
   },
   {
     id: 2,
     logo: '',
     employer: 'PRESTIGE ENGLISH SCHOOL',
-    startDate: '2024/03/26',
-    endDate: '2024/03/26',
+    startDate: '2024/07/15',
+    endDate: '2024/08/28',
     jobTitle: 'FREELANCE',
     descES: '',
     descEn: '',
     tasks: [
-      { id: 1, taskDescES: '', taskDescEN: '' },
-      { id: 2, taskDescES: '', taskDescEN: '' },
-      { id: 3, taskDescES: '', taskDescEN: '' },
-      { id: 4, taskDescES: '', taskDescEN: '' },
+      {
+        id: 1,
+        taskDescES:
+          'Instalación y configuración del backend y frontend de DSpace.',
+        taskDescEN: `Install and configure DSpace backend and frontend for it's proper operation.`,
+      },
+      {
+        id: 2,
+        taskDescES:
+          'Personalización de estilos e información en base a la escuela.',
+        taskDescEN:
+          'Customization of the styles and information based on the school branding.',
+      },
+      {
+        id: 3,
+        taskDescES: 'Despliegue del repositorio digital.',
+        taskDescEN: 'Deployment of the digital repository.',
+      },
     ],
   },
   {
     id: 3,
     logo: '',
     employer: 'SECRETARÍA DE FINANZAS Y ADMINISTRACIÓN DEL ESTADO DE MICHOACÁN',
-    startDate: '2024/03/26',
-    endDate: '2024/03/26',
+    startDate: '2023/01/05',
+    endDate: '2023/06/05',
     jobTitle: 'FULLSTACK DEVELOPER',
     descES: '',
     descEn: '',
     tasks: [
-      { id: 1, taskDescES: '', taskDescEN: '' },
-      { id: 2, taskDescES: '', taskDescEN: '' },
-      { id: 3, taskDescES: '', taskDescEN: '' },
-      { id: 4, taskDescES: '', taskDescEN: '' },
+      {
+        id: 1,
+        taskDescES: 'Migración de la base de datos de Access a MySQL.',
+        taskDescEN: 'Migrate database from Access to MySQL.',
+      },
+      {
+        id: 2,
+        taskDescES: 'Desarrollo del sitio web usando laravel',
+        taskDescEN:
+          'Participate in the development of the website using Laravel.',
+      },
+      {
+        id: 3,
+        taskDescES:
+          'Definición de estándares de código dentro del proyecto y participación en revisión de pull requests.',
+        taskDescEN:
+          'Define code standards among the project and participate in pull requests reviews.',
+      },
+      {
+        id: 4,
+        taskDescES: 'Trabajé con Jira y control de versiones (GIT).',
+        taskDescEN: 'Work with Jira and versioning systems (GIT).',
+      },
     ],
   },
   {
     id: 4,
     logo: '',
     employer: 'GRUPO LIAS',
-    startDate: '2024/03/26',
-    endDate: '2024/03/26',
+    startDate: '2022/03/26',
+    endDate: '2022/03/26',
     jobTitle: 'FULLSTACK DEVELOPER',
-    descES: '',
-    descEn: '',
+    descES:
+      'Este proyecto fue llevado a cabo por un equipo de 5 personas, el cual se desarrollo un CMS (Sistema de Gestión de Contenidos) para una empresa de servicios técnicos residenciales. Este cliente fue conseguido por una maestra, la cual nos brindó el apoyo para tener nuestro primer acercamiento en el mundo laboral, de tal manera que pudieramos aprender, trabajar y desarrollar el proyecto para un cliente real.',
+    descEn: `This project was developed by me and 4 more persons. We've developed a CMS (Content Management System) to allow the user manage it's own information which is displayed on the website. This software was about a website for technical residential services. This project was my first aproach into the real world since a teacher brought us this opportunity so we can learn a lot of things from it.`,
     tasks: [
-      { id: 1, taskDescES: '', taskDescEN: '' },
-      { id: 2, taskDescES: '', taskDescEN: '' },
-      { id: 3, taskDescES: '', taskDescEN: '' },
-      { id: 4, taskDescES: '', taskDescEN: '' },
+      {
+        id: 1,
+        taskDescES:
+          'Definición de requisitos de software de acuerdo a las especificaciones del cliente.',
+        taskDescEN:
+          'Define software specifications according to the client requirements.',
+      },
+      {
+        id: 2,
+        taskDescES: 'Desarrollo del frontend y backend con Django.',
+        taskDescEN:
+          'Participate in the development of backend and frontend with Django.',
+      },
+      {
+        id: 3,
+        taskDescES:
+          'Diseño de la base de datos con SQLite de acuerdo a las especificaciones de software.',
+        taskDescEN: 'Designed SQL database based on software specifications.',
+      },
+      {
+        id: 4,
+        taskDescES:
+          'Trabajé con la metodología SCRUM y control de versiones (GIT).',
+        taskDescEN: 'Work with SCRUM methodology and versioning systems(GIT).',
+      },
     ],
   },
 ];
