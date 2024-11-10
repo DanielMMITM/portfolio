c1faa2496e9659df498838eebdea35c50d890a75
Edgarm <edgar.martinez@canteradigital.mx>
2024-11-10 17:50:52 -0600

    Update - update project card borders

--- src/components/Tabs/Projects.tsx
+++ src/components/Tabs/Projects.tsx
@@ -15,13 +15,13 @@ export const Projects = () => {
           <img
             src={banner}
             alt=" Project banner"
-            className="rounded-tl-xl rounded-tr-xl h-72 w-full object-cover"
+            className="rounded-tl-xl rounded-tr-xl h-72 w-full object-cover object-left-top"
           />
           <div className="flex flex-wrap mt-4 px-4 text-gray-900 dark:text-gray-300">
             {technologies.map(({ id, name, icon }) => (
               <div
                 key={id}
-                className="flex m-1 p-2 border rounded-xl border-secondaryLight items-center"
+                className="flex m-1 p-2 border rounded-xl border-primary dark:border-white items-center"
               >
                 <img src={icon} alt={`${name} icon`} className="w-4 h-4 mr-2" />
                 <span className="text-inherit">{name}</span>
