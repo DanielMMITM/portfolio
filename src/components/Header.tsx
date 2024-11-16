0b19ada0005e9a2849858178261b5f74017b0be4
Edgarm <edgar.martinez@canteradigital.mx>
2024-11-15 22:32:34 -0600

    Update - set icon instead of text to change the theme mode

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,6 @@
 import { useAppContext } from '@/context/AppContext';
+import darkModeIcon from '@/assets/icons/darkMode.svg';
+import lightModeIcon from '@/assets/icons/lightMode.svg';
 
 export const Header = () => {
   const { english, toggleLanguage, darkMode, toggleDarkMode } = useAppContext();
@@ -6,15 +8,19 @@ export const Header = () => {
   return (
     <header>
       <nav className="flex justify-center md:justify-end items-center z-10">
-        <ul className="flex gap-x-6">
+        <ul className="flex gap-x-6 items-center">
           <li>
             <button type="button" onClick={toggleLanguage}>
               {english ? 'ES' : 'EN'}
             </button>
           </li>
           <li>
-            <button type="button" onClick={toggleDarkMode}>
-              {darkMode ? 'Clear mode' : 'Dark Mode'}
+            <button className="flex" type="button" onClick={toggleDarkMode}>
+              <img
+                src={darkMode ? lightModeIcon : darkModeIcon}
+                alt={`${darkMode ? 'Light mode' : 'Dark mode'} icon`}
+                className={`${darkMode && 'invert'} icon w-5`}
+              />
             </button>
           </li>
         </ul>
