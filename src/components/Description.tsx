d1c59839ceeafcc19d8339b2b9a9695e19db59ec
Edgarm <edgar.martinez@canteradigital.mx>
2024-11-15 22:32:03 -0600

    Add - add contact section

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -2,6 +2,7 @@ import { SELF_DESCRIPTION_EN } from '@/constants/english';
 import { SELF_DESCRIPTION_ES } from '@/constants/spanish';
 import { useAppContext } from '@/context/AppContext';
 import Profile from '@/assets/images/profile.webp';
+import { contactInfo } from '@/data/contactInfo';
 
 export const Description = () => {
   const { english } = useAppContext();
@@ -19,6 +20,22 @@ export const Description = () => {
         <h1 className="min-h-14 text-5xl font-extrabold bg-clip-text text-transparent text-center md:text-start bg-gradient-to-r from-secondaryLight to-tertiaryLight dark:from-secondary dark:to-tertiary">
           Edgar Daniel Martínez Morales
         </h1>
+        <div className="-mt-4">
+          <h2 className="text-md font-bold">Contact</h2>
+          <div className="flex gap-2 mt-2">
+            {contactInfo?.map(({ id, icon, name, link, mailFlag }) => (
+              <a
+                key={id}
+                className="flex gap-2 py-1 px-2 rounded-lg dark:bg-secondary/25  dark:hover:bg-secondary/65 bg-secondaryLight/25 hover:bg-secondaryLight/55 shadow-inner"
+                href={mailFlag ? `mailto:${link}` : link}
+                target="_blank"
+              >
+                <img src={icon} alt="" className="w-3" />
+                <span className="text-sm">{name}</span>
+              </a>
+            ))}
+          </div>
+        </div>
         <p className="min-w-[30dvw] mb-20">
           {english ? SELF_DESCRIPTION_EN : SELF_DESCRIPTION_ES}
         </p>
---
+++ src/data/contactInfo.ts
@@ -0,0 +1,25 @@
+import gmail from '@/assets/icons/gmail.svg';
+import github from '@/assets/icons/github.svg';
+import linkedin from '@/assets/icons/linkedin.svg';
+
+export const contactInfo = [
+  {
+    id: 1,
+    icon: gmail,
+    name: 'Gmail',
+    mailFlag: true,
+    link: 'edgar.danmm@gmail.com',
+  },
+  {
+    id: 2,
+    icon: linkedin,
+    name: 'LinkedIn',
+    link: 'https://www.linkedin.com/in/edgar-mm/',
+  },
+  {
+    id: 3,
+    icon: github,
+    name: 'GitHub',
+    link: 'https://github.com/DanielMMITM',
+  },
+];
