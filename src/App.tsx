5fdbdc29415e96e57e4627bb943d87092a7619c1
Edgarm <edgar.martinez@canteradigital.mx>
2024-11-15 22:34:37 -0600

    Update - uncomment meteors component and delete border layout

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,8 @@ export const App = () => {
   return (
     <AppContextProvider>
       <div className="relative flex w-full flex-col overflow-hidden text-tertiaryLight dark:bg-primary dark:text-tertiary border-3 min-h-[100dvh] ">
-        <div className="flex flex-col p-6 mx-8 md:mx-40 gap-y-7 border-2">
-          {/* <Meteors number={20} /> */}
+        <div className="flex flex-col p-6 mx-8 md:mx-40 gap-y-7">
+          <Meteors number={20} />
           <Header />
           <Description />
           <Tabs />
