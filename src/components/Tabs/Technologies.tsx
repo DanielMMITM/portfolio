bbe4a12f73a5209c06ee4219672c268e672594db
Edgarm <edgar.martinez@canteradigital.mx>
2024-11-15 19:57:58 -0600

    Update - update layout and add technologies info

--- src/components/Tabs/Technologies.tsx
+++ src/components/Tabs/Technologies.tsx
@@ -1,6 +1,5 @@
 import { technologies } from '@/data/technologies';
 import { Swiper, SwiperSlide } from 'swiper/react';
-// import { Autoplay, Navigation } from 'swiper/modules';
 
 import 'swiper/css';
 import 'swiper/css/autoplay';
@@ -8,26 +7,33 @@ import 'swiper/css/navigation';
 import { swiperParams } from '@/data/swiper';
 import { MagicCard } from '../MagicCard';
 import { useAppContext } from '@/context/AppContext';
+import { TECHNOLOGIES_DESC_EN } from '@/constants/english';
+import { TECHNOLOGIES_DESC_ES } from '@/constants/spanish';
 
 export const Technologies = () => {
-  const { darkMode } = useAppContext();
+  const { darkMode, english } = useAppContext();
 
   return (
-    <Swiper {...swiperParams}>
-      {technologies.map(({ id, icon, name }) => (
-        <SwiperSlide key={id} className="flex mt-10 h-auto">
-          <MagicCard
-            key={id}
-            className="flex-col shadow-2xl justify-center items-center p-4 w-full text-center"
-            gradientColor={darkMode ? '#f4619755' : '#2364aa50'}
-          >
-            <img width="64" height="64" alt="" src={icon} />
-            <figcaption className="text-sm font-medium dark:text-white mt-2">
-              {name}
-            </figcaption>
-          </MagicCard>
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <section className="mt-10">
+      <p className="mb-10">
+        {english ? TECHNOLOGIES_DESC_EN : TECHNOLOGIES_DESC_ES}
+      </p>
+      <Swiper {...swiperParams}>
+        {technologies.map(({ id, icon, name }) => (
+          <SwiperSlide key={id} className="flex h-auto">
+            <MagicCard
+              key={id}
+              className="flex-col shadow-2xl justify-center items-center p-4 w-full text-center"
+              gradientColor={darkMode ? '#f4619755' : '#2364aa50'}
+            >
+              <img width="64" height="64" alt="" src={icon} />
+              <figcaption className="text-sm font-medium dark:text-white mt-2">
+                {name}
+              </figcaption>
+            </MagicCard>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </section>
   );
 };
