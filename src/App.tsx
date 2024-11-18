40b6fcac6d6badd22c2377be25965f83cffd33dd
Edgarm <edgar.martinez@canteradigital.mx>
2024-11-18 16:34:36 -0600

    Clean up

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ document.documentElement.classList.toggle('dark');
 export const App = () => {
   return (
     <AppContextProvider>
-      <div className="relative flex w-full flex-col overflow-hidden text-tertiaryLight dark:bg-primary dark:text-tertiary border-3 min-h-[100dvh] ">
+      <div className="relative flex w-full flex-col overflow-hidden text-tertiaryLight dark:bg-primary dark:text-tertiary border-3 min-h-[100dvh]">
         <div className="flex flex-col p-6 mx-8 md:mx-40 gap-y-7">
           <Meteors number={20} />
           <Header />
