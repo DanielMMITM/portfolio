20ca3b2879e16e7274f44e80058c30daa2bb9547
Edgarm <edgar.martinez@canteradigital.mx>
2025-04-11 21:11:18 -0600

    Update - update personal info

--- src/constants/spanish.ts
+++ src/constants/spanish.ts
@@ -1,7 +1,7 @@
 export const SELF_DESCRIPTION_ES =
-  'Mi nombre es Edgar Martínez, tengo 24 años y soy ingeniero en sistemas computacionales Llevo algunos años trabajando en desarrollo de software, sobre todo en el área de FrontEnd. También tengo una especialidad en seguridad de la información, así que siempre trato de aplicar esos conocimientos para hacer que mi código sea más seguro y confiable. Me encanta aprender cosas nuevas sobre tecnología. Disfruto leer novedades, pero lo que más me gusta es poner en práctica lo que aprendo.';
+  'Mi nombre es Edgar Martínez, tengo 24 años y soy ingeniero en sistemas computacionales. Llevo algunos años trabajando en desarrollo de software, sobre todo en el área de frontend. También tengo una especialidad en seguridad de la información, así que siempre trato de aplicar esos conocimientos para hacer que mi código sea más seguro y confiable. Me encanta aprender cosas nuevas sobre tecnología. Disfruto leer novedades, pero lo que más me gusta es poner en práctica lo que aprendo.';
 export const WORKING_ON_ES =
-  'Actualmente me encuentro aprendiendo sobre AWS, preparándome para la certificación de desarrollador con el programa AWS Educate. Además, estoy trabajando como freelance en un software administrativo y, cuando tengo tiempo, me gusta hacer proyectos personales para seguir mejorando mis habilidades.';
+  'Actualmente me encuentro aprendiendo sobre AWS, preparándome para la certificación de desarrollador con el programa AWS Educate. Además, estoy trabajando como freelance en el desarrollo de un software administrativo y, cuando tengo tiempo, me gusta hacer proyectos personales para seguir mejorando mis habilidades.';
 export const EXPERIENCE_ES = 'Experiencia';
 export const PROJECTS_ES = 'Proyectos Personales';
 export const TECHNOLOGIES_ES = 'Tecnologías';
